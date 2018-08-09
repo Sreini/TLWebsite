c5094c31388ec14d6245e0609bd7acfa04a1a191
Eirini Galati <eirini.galati@gmail.com>
2018-08-09 14:37:35 +0200

    began working on the database

---
+++ TLWebsite/TLWebsite/Models/PersonContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TLWebsite.Models
+{
+    public class PersonContext   : DbContext
+    {
+    }
+}
