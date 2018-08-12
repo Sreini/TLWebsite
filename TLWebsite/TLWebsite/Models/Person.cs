4f7fd4021d245b25d9f88015e84b213b65d5f277
Eirini Galati <eirini.galati@gmail.com>
2018-08-12 10:19:27 +0200

    added person model

---
+++ TLWebsite/TLWebsite/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TLWebsite.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string Role { get; set; }
+
+        public Photo Photo { get; set; }
+        
+        public DateTime DOB { get; set; }
+
+        public ICollection<Project> Projects { get; set; }  
+
+        public ICollection<Course> Courses { get; set; }
+
+        public ICollection<Certificate> Certificates { get; set; }
+    }
+}
--- TLWebsite/TLWebsite/Models/PersonContext.cs
+++ TLWebsite/TLWebsite/Models/PersonContext.cs
@@ -6,7 +6,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TLWebsite.Models
 {
-    public class PersonContext   : DbContext
+    public class PersonContext : DbContext
     {
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasData(
+                new Person
+                {
+
+                });
+        } 
+
+        public DbSet<Person> Person { get; set; }
+
+        public DbSet<Photo> Photo { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Certificate> Certificates { get; set; }
+
     }
 }
